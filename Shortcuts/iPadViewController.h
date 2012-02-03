3c64122c9cd798e0c93675f7055653127ec05585
James Reyna <james.r.reyna@us.army.mil>
2012-02-03 10:15:55 -0600

    finshed product

--- Shortcuts/iPadViewController.h
+++ Shortcuts/iPadViewController.h
@@ -8,20 +8,31 @@
 
 #import <UIKit/UIKit.h>
 
-@interface iPadViewController : UIViewController
+@class DictionaryViewController;
+@class SearchableShortcutsViewController; @class FavoritesViewController;
+
+@class SettingsViewController;
+@interface iPadViewController : UIViewController { 
+    DictionaryViewController * _menusViewController;
+    DictionaryViewController * _keysViewController; 
+    SearchableShortcutsViewController * _allShortcutsViewController; 
+    FavoritesViewController * _favoritesViewController; 
+    SettingsViewController * _settingsViewController; 
+    UINavigationController * _menusNav;
+    UINavigationController * _keysNav;
+    UINavigationController * _allNav; 
+}
+@property (strong, nonatomic) IBOutlet UIView *leftView; 
+@property (strong, nonatomic) IBOutlet UIView *middleView; 
+@property (strong, nonatomic) IBOutlet UIView *rightView; 
+@property (strong, nonatomic) IBOutlet UIView *decorationView; 
+@property (strong, nonatomic) IBOutlet UIView *bottomView; 
+@property (strong, nonatomic) IBOutlet UIImageView *favoritesImageView;
+@property (strong, nonatomic) IBOutlet UIButton *favoritesButton; 
+@property (strong, nonatomic) IBOutlet UIButton *settingsButton; 
+@property (strong, nonatomic) IBOutlet UIImageView *bgImageView; 
+
 - (IBAction)favoritesButtonTapped:(id)sender;
 - (IBAction)settingsButtonTapped:(id)sender;
-
-@property (strong, nonatomic) IBOutlet UIView *leftView;
-@property (strong, nonatomic) IBOutlet UIView *middleView;
-@property (strong, nonatomic) IBOutlet UIView *rightView;
-@property (strong, nonatomic) IBOutlet UIView *decorationView;
-@property (strong, nonatomic) IBOutlet UIView *bottomView;
-@property (strong, nonatomic) IBOutlet UIImageView *favoritesImageView;
-@property (strong, nonatomic) IBOutlet UIButton *favoritesButton;
-@property (strong, nonatomic) IBOutlet UIButton *settingsButton;
-@property (strong, nonatomic) IBOutlet UIImageView *bgImageView;
-
-
 
 @end
--- Shortcuts/iPadViewController.m
+++ Shortcuts/iPadViewController.m
@@ -7,6 +7,11 @@
 //
 
 #import "iPadViewController.h"
+#import "ShortcutsDatabase.h"
+#import "DictionaryViewController.h"
+#import "SearchableShortcutsViewController.h" 
+#import "FavoritesViewController.h"
+#import "SettingsViewController.h"
 
 @implementation iPadViewController
 @synthesize leftView;
@@ -42,8 +47,38 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    self.bgImageView.image = [[UIImage imageNamed:@"bg_200"]
+                              resizableImageWithCapInsets:UIEdgeInsetsMake(0, 0, 0, 0)];
     
-
+    _menusViewController = [[DictionaryViewController alloc] initWithNibName:nil bundle:nil];
+    _menusViewController.navigationItem.title = @"Shortcuts By Menu"; 
+    _menusViewController.dict =
+    [ShortcutsDatabase sharedDatabase].shortcutsByMenu; 
+    _menusViewController.keys = [ShortcutsDatabase sharedDatabase].menusArray;
+    _keysViewController = [[DictionaryViewController alloc] initWithNibName:nil bundle:nil];
+    _keysViewController.navigationItem.title = @"Shortcuts By Key"; 
+    _keysViewController.dict =
+    [ShortcutsDatabase sharedDatabase].shortcutsByKey;
+    _allShortcutsViewController = [[SearchableShortcutsViewController alloc] initWithNibName:nil bundle:nil];
+    _allShortcutsViewController.navigationItem.title = @"All Shortcuts"; 
+    _allShortcutsViewController.shortcutsDict = [ShortcutsDatabase sharedDatabase].shortcutsByKey;
+    _favoritesViewController = [[FavoritesViewController alloc] initWithNibName:nil bundle:nil];
+    _settingsViewController = [[SettingsViewController alloc] initWithNibName:nil bundle:nil];
+    _menusNav = [[UINavigationController alloc] initWithRootViewController:_menusViewController];
+    _keysNav = [[UINavigationController alloc] initWithRootViewController:_keysViewController];
+    _allNav = [[UINavigationController alloc]
+               initWithRootViewController:_allShortcutsViewController];
+    
+    _menusNav.view.frame = leftView.bounds; _keysNav.view.frame = middleView.bounds; _favoritesViewController.view.frame = bottomView.bounds; _settingsViewController.view.frame = bottomView.bounds; 
+    _allNav.view.frame = rightView.bounds;
+    [leftView addSubview:_menusNav.view];
+    [middleView addSubview:_keysNav.view];
+    [bottomView addSubview:_favoritesViewController.view]; 
+    [rightView addSubview:_allNav.view];
+    
+    [self addChildViewController:_menusNav]; [_menusNav didMoveToParentViewController:self]; [self addChildViewController:_keysNav];
+    [_keysNav didMoveToParentViewController:self];
+    [self addChildViewController:_favoritesViewController]; [_favoritesViewController didMoveToParentViewController:self];
 }
 
 - (void)viewDidUnload
@@ -68,10 +103,33 @@
 	return YES;
 }
 
-- (IBAction)favoritesButtonTapped:(id)sender {
+- (IBAction)favoritesButtonTapped:(id)sender { [[ShortcutsDatabase sharedDatabase] playClick];
+    [self addChildViewController:_favoritesViewController]; 
+    [self transitionFromViewController:_settingsViewController
+     toViewController:_favoritesViewController duration:0.5 
+    options:UIViewAnimationOptionTransitionFlipFromBottom animations:^{
+        [_settingsViewController.view removeFromSuperview]; 
+        _favoritesViewController.view.frame = bottomView.bounds; 
+        [bottomView addSubview:_favoritesViewController.view];
+    } completion:^(BOOL finished) { [_favoritesViewController
+    didMoveToParentViewController:self]; 
+        [_settingsViewController removeFromParentViewController];
+    }]; 
 }
 
 - (IBAction)settingsButtonTapped:(id)sender {
+    [[ShortcutsDatabase sharedDatabase] playClick];
+    [self addChildViewController:_settingsViewController]; 
+    [self transitionFromViewController:_favoritesViewController
+    toViewController:_settingsViewController duration:0.5 options:
+     UIViewAnimationOptionTransitionFlipFromBottom animations:^{
+    [_favoritesViewController.view removeFromSuperview]; 
+         _settingsViewController.view.frame = bottomView.bounds; 
+         [bottomView addSubview:_settingsViewController.view];
+    } completion:^(BOOL finished) { [_settingsViewController
+    didMoveToParentViewController:self]; 
+        [_favoritesViewController removeFromParentViewController];
+    }];
 }
 
 - (void)layoutForOrientation:(UIInterfaceOrientation)interfaceOrientation 
